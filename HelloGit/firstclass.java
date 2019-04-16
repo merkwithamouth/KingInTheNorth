b5479825007b0c1632bfa4447d76f76737a7ad60
fazlicodes <fazlimaam@gmail.com>
2019-04-16 10:45:21 +0530

    second commit(pc2)

---
+++ HelloGit/firstclass.java
@@ -0,0 +1,15 @@
+
+public class firstclass {
+
+	public firstclass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello from this side!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
